5c3d3a2c8025768aac986a9f8244d4a3f85cc505
ahmetcgl <57807015+ahmetcgl83@users.noreply.github.com>
2023-03-19 20:52:11 +0100

    Update app.js

--- homework/hackyourtemperature/app.js
+++ homework/hackyourtemperature/app.js
@@ -1,29 +1,30 @@
 import { API_KEY } from "./sources/keys.js";
 import express from "express";
+import fetch from "node-fetch";
 
 const app = express();
 app.use(express.json());
 
 app.get("/", (req, res) => {
-	res.send("hello from backend to frontend!");
+  res.send("hello from backend to frontend!");
 });
 
-app.post("/weather/:cityName", async (req, res) => {
-	const cityName = req.params.cityName;
+app.post("/weather", async (req, res) => {
+  const cityName = req.body.cityName;
 
-	const response = await fetch(
-		`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${API_KEY}&units=metric`
-	);
-	const data = await response.json();
-	if (typeof data.name === "undefined") {
-		res.status(400);
-		res.send({ weatherText: "City is not found!" });
-		return;
-	}
+  const response = await fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${API_KEY}&units=metric`
+  );
+  const data = await response.json();
+  if (typeof data.name === "undefined") {
+    res.status(400);
+    res.send({ weatherText: "City is not found!" });
+    return;
+  }
 
-	const info = { city: data.name, temperature: `${data.main.temp} Celsius` };
-	res.status(200);
-	res.send(info);
+  const info = { city: data.name, temperature: `${data.main.temp} Celsius` };
+  res.status(200);
+  res.send(info);
 });
 
 export default app;
